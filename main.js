5fd6738292ecdb0101546071cd08db226f1c1226
Somu198365 <73246101+Somu198365@users.noreply.github.com>
2020-12-01 20:43:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+menu_list_array = ["Chicken Tanduri pizza"
+
+                                ]
+   document.getElementById.toString(var_menuList);
+   print.name(chicken-Pizza);
+   print.name(mutton-Pizza);
+   print.name(paneer-Pizza);
+   print.name(veg-Pizza);
+   document.getElementById(chi.png);
+   document.getElementById(veg.png);
+   document.getElementById(pan.png);
+   document.getElementById(mut.png);
+   RTCIceCandidatePairChangedEvent
+   function getmenu(){
+    window.location.href = "http://127.0.0.1:5500/#addTopings";
+   }
+   function redict(){
+    window.location.href = "https://www.dominos.co.in/menu/veg-pizzas";
+   }
+   function redict1(){
+    window.location.href = "https://www.dominos.co.in/menu/non-veg-pizzas";
+   }
